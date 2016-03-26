b607179698fe358a5bffe6cf52489209519e1280
Peter Elliott <p.elliott332@gmail.com>
2016-03-26 11:17:52 -0600

    removed shuffle algos from Deck

--- cards.py
+++ cards.py
@@ -79,48 +79,47 @@ class Card:
         return self.card_num >= other.card_num
 
 
+def randShuffle(deck):
+    temp_deck = deck[:]
+    del deck[:]
+
+    while len(temp_deck) > 0:
+        deck.append(temp_deck.pop(rand(0, len(temp_deck)-1)))
+
+
+def realShuffle(deck):
+    cut = len(deck)//2 + normalDist(-CUT_ERROR, CUT_ERROR, 3)
+    a = deck[:cut]
+    b = deck[cut:]
+    del deck[:]
+
+    use_a = randBool()
+    cutoff = rand(0, SHUFFLE_START_ERROR)
+
+    if use_a:
+        deck.extend(a[-cutoff:])
+        del a[-cutoff:]
+    else:
+        deck.extend(b[-cutoff:])
+        del b[-cutoff:]
+
+    while len(a) > 0 or len(b) > 0:
+        use_a = not use_a
+        shuffle_error = rand(0, SHUFFLE_ERROR) + 1
+        if use_a:
+            if len(a) > 0:
+                deck.extend(a[-shuffle_error:])
+                del a[-shuffle_error:]
+        else:
+            if len(b) > 0:
+                deck.extend(b[-shuffle_error:])
+                del b[-shuffle_error:]
+
+
 class Deck(list):
     def __init__(self, cards=standardDeck()):
         super().__init__()
-        self += list(cards)
-
-    def realShuffle(self):
-        cut = len(self)//2 + normalDist(-CUT_ERROR, CUT_ERROR, 3)
-        a = self[:cut]
-        b = self[cut:]
-        temp_deck = []
-
-        use_a = randBool()
-        cutoff = rand(0, SHUFFLE_START_ERROR)
-
-        if use_a:
-            temp_deck = a[-cutoff:] + temp_deck
-            del a[-cutoff:]
-        else:
-            temp_deck = b[-cutoff:] + temp_deck
-            del b[-cutoff:]
-
-        while len(a) > 0 or len(b) > 0:
-            use_a = not use_a
-            shuffle_error = rand(0, SHUFFLE_ERROR) + 1
-            if use_a:
-                if len(a) > 0:
-                    temp_deck = a[-shuffle_error:] + temp_deck
-                    del a[-shuffle_error:]
-            else:
-                if len(b) > 0:
-                    temp_deck = b[-shuffle_error:] + temp_deck
-                    del b[-shuffle_error:]
-
-        del self[:]
-        self += temp_deck
-
-    def randShuffle(self):
-        temp_deck = self[:]
-        del self[:]
-
-        while len(temp_deck) > 0:
-            self += [temp_deck.pop(rand(0, len(temp_deck)-1))]
+        self += list(cards)  
 
     def deal(self):
         return self.pop(0)
